528d672fec90c40ce33c6824f0221d187c010684
Karl Seguin <karl@openmymind.io>
2014-01-02 12:07:02 +0800

    added the plus filter

---
+++ filters/plus.go
@@ -0,0 +1,69 @@
+package filters
+
+import (
+	"strconv"
+	"time"
+)
+
+// Creates a plus filter
+func PlusFactory(parameters []string) Filter {
+	if len(parameters) == 0 {
+		return (&IntPlusFilter{1, "1"}).Plus
+	}
+	if i, err := strconv.Atoi(parameters[0]); err == nil {
+		return (&IntPlusFilter{i, parameters[0]}).Plus
+	}
+	return (&StringPlusFilter{parameters[0]}).Plus
+}
+
+// Plus filter for numbers
+type IntPlusFilter struct {
+	plus       int
+	plusString string
+}
+
+func (p *IntPlusFilter) Plus(input interface{}) interface{} {
+	switch typed := input.(type) {
+	case int:
+		return typed + p.plus
+	case float64:
+		return typed + float64(p.plus)
+	case uint:
+		return typed + uint(p.plus)
+	case int64:
+		return typed + int64(p.plus)
+	case uint64:
+		return typed + uint64(p.plus)
+	case time.Time:
+		return typed.Add(time.Minute * time.Duration(p.plus))
+	case string:
+		return plusString(typed, p.plus, p.plusString)
+	case []byte:
+		return plusString(string(typed), p.plus, p.plusString)
+	default:
+		return input
+	}
+}
+
+func plusString(s string, plus int, plusString string) interface{} {
+	if i, err := strconv.Atoi(s); err == nil {
+		return i + plus
+	}
+	return s + plusString
+}
+
+// plus filter for strings
+type StringPlusFilter struct {
+	plus string
+}
+
+func (p *StringPlusFilter) Plus(input interface{}) interface{} {
+	switch typed := input.(type) {
+	case string:
+		return typed + p.plus
+	case []byte:
+		return string(typed) + p.plus
+	default:
+		return input
+	}
+}
---
+++ filters/plus_test.go
@@ -0,0 +1,62 @@
+package filters
+
+import (
+	"github.com/karlseguin/gspec"
+	"time"
+	"testing"
+)
+
+func TestPlusAnIntToAnInt(t *testing.T) {
+	spec := gspec.New(t)
+	filter := PlusFactory([]string{"5"})
+	spec.Expect(filter(43).(int)).ToEqual(48)
+}
+
+func TestPlusAnIntToAFloat(t *testing.T) {
+	spec := gspec.New(t)
+	filter := PlusFactory([]string{"5"})
+	spec.Expect(filter(43.2).(float64)).ToEqual(48.2)
+}
+
+func TestPlusAnIntToATime(t *testing.T) {
+	spec := gspec.New(t)
+	now := time.Now()
+	filter := PlusFactory([]string{"7"})
+	spec.Expect(filter(now).(time.Time)).ToEqual(now.Add(time.Minute * 7))
+}
+
+func TestPlusAnIntToAStringAsAnInt(t *testing.T) {
+	spec := gspec.New(t)
+	filter := PlusFactory([]string{"7"})
+	spec.Expect(filter("33").(int)).ToEqual(40)
+}
+
+func TestPlusAnIntToBytesAsAnInt(t *testing.T) {
+	spec := gspec.New(t)
+	filter := PlusFactory([]string{"7"})
+	spec.Expect(filter([]byte("34")).(int)).ToEqual(41)
+}
+
+func TestPlusAnIntToAStringAsAString(t *testing.T) {
+	spec := gspec.New(t)
+	filter := PlusFactory([]string{"7"})
+	spec.Expect(filter("abc").(string)).ToEqual("abc7")
+}
+
+func TestPlusAnIntToBytesAsAString(t *testing.T) {
+	spec := gspec.New(t)
+	filter := PlusFactory([]string{"8"})
+	spec.Expect(filter([]byte("abb")).(string)).ToEqual("abb8")
+}
+
+func TestPlusAStringToBytes(t *testing.T) {
+	spec := gspec.New(t)
+	filter := PlusFactory([]string{"xx"})
+	spec.Expect(filter([]byte("abb")).(string)).ToEqual("abbxx")
+}
+
+func TestPlusAStringToString(t *testing.T) {
+	spec := gspec.New(t)
+	filter := PlusFactory([]string{"axx9"})
+	spec.Expect(filter("dasdb").(string)).ToEqual("dasdbaxx9")
+}
