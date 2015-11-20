db053523fb49ea07fbbfc7c02cec9abc831db10c
Hafiz Ismail <sgk.sprm@gmail.com>
2015-11-20 18:39:53 +0800

    Implemented `with` support for `include` tag - https://docs.shopify.com/themes/liquid-documentation/tags/theme-tags#include

--- tags/include.go
+++ tags/include.go
@@ -7,20 +7,61 @@ import (
 
 // Creates an include tag
 func IncludeFactory(p *core.Parser, config *core.Configuration) (core.Tag, error) {
-	value, err := p.ReadValue()
+	includeName, err := p.ReadValue()
 	if err != nil {
 		return nil, err
 	}
-	if value == nil {
+	if includeName == nil {
 		return nil, p.Error("Invalid include value")
 	}
+
+	valuesMap := map[string]core.Value{}
+
+	next := p.SkipSpaces()
+	if next == 'w' {
+		name := p.ReadName()
+		if name == "with" {
+			value, _ := p.ReadValue()
+			includeNameString := core.ToString(includeName.Resolve(nil))
+			valuesMap[includeNameString] = value
+		}
+	}
+	for {
+		if next != ',' {
+			break
+		}
+		p.Forward()
+
+		// has one-line
+		name := p.ReadName()
+
+		next = p.SkipSpaces()
+		if next == ':' {
+			p.Forward()
+			value, err := p.ReadValue()
+			if err != nil {
+				continue
+			}
+			valuesMap[name] = value
+		}
+
+		next = p.SkipSpaces()
+
+	}
+
 	p.SkipPastTag()
-	return &Include{value, config.GetIncludeHandler()}, nil
+
+	return &Include{
+		includeName: includeName,
+		handler:     config.GetIncludeHandler(),
+		valuesMap:   valuesMap,
+	}, nil
 }
 
 type Include struct {
-	value   core.Value
-	handler core.IncludeHandler
+	includeName core.Value
+	handler     core.IncludeHandler
+	valuesMap   map[string]core.Value
 }
 
 func (i *Include) AddCode(code core.Code) {
@@ -32,9 +73,16 @@ func (i *Include) AddSibling(tag core.Tag) error {
 }
 
 func (i *Include) Execute(writer io.Writer, data map[string]interface{}) core.ExecuteState {
-	if i.handler != nil {
-		i.handler(core.ToString(i.value.Resolve(data)), writer, data)
+
+	// merge local values
+	for key, val := range i.valuesMap {
+		data[key] = val.Resolve(data)
 	}
+
+	if i.handler != nil {
+		i.handler(core.ToString(i.includeName.Resolve(data)), writer, data)
+	}
+
 	return core.Normal
 }
 
